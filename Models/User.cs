dd6a798121202bfb8e182ad4b649157dde5f6b09
pizzabakker <b.schulpen@student.fontys.nl>
2021-02-03 09:57:10 +0100

    user model

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Models
+{
+    class User{
+
+        // variables
+        private int id;
+        private string name;
+
+        // constuctor
+        public User(String name) {
+            this.name = name;
+        }
+
+        // functions
+
+
+
+    }
